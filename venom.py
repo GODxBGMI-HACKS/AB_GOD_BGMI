e3dbdedb58f398e8f4f762a8cb6a02acbc863702
GODxBGMI-HACKS <trpnaruto01@gmail.com>
2024-07-12 22:28:39 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,7 +4,7 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = '7353106103:AAEmWPOELbGBOlzJiKX-LUkS-WcHcqTYphc'
+API_TOKEN = '6631501015:AAG0ncg9olQIhxQAdRuZWqRbHnJfGSXi9z8'
 ADMIN_ID = '1132426169'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
